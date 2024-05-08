eec467792656b11561cf4c401829073e1ecf72db
dascir <dascoliir@gmail.com>
2024-05-08 10:07:16 +0000

    giorno 3

---
+++ ex14.1_arrei_stringhe.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+char nome[5]={'s','a','e','r','\0'};
+int i=o
+while (nome[i]!='\0')
+{
+    printf("%c", nome[i])
+    i=i+1
+}
+printf("%s", nome);
+
+// char nome2[]="ciao fratm";
+// char nomeutente[50];
+// scanf("%s", nomeutente)    -> & non ci va
+///scanf("%[^\n]s", ...)   // per inserire spazi
+
+
+
+
+}
+
+
+
+
